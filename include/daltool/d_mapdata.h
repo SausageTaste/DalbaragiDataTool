5ef0e8380791875b3d11f7d2cebb91f2913d8666
Sungmin Woo <woos8899@gmail.com>
2020-03-19 16:32:30 +0900

    Added data structures for map v2.

--- include/daltool/d_mapdata.h
+++ include/daltool/d_mapdata.h
@@ -3,12 +3,14 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <memory>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 
 
 namespace daldata {
+
     namespace v1 {
 
         struct Mesh {
@@ -69,4 +71,79 @@ namespace daldata {
         };
 
     }
+
+
+    namespace v2 {
+
+        class Material {
+
+        public:
+            std::string m_albedoMap, m_roughnessMap, m_metallicMap;
+            float m_roughness = 0.5f, m_metallic = 1.f;
+
+        };
+
+        class Mesh {
+
+        public:
+            virtual ~Mesh(void) = default;
+
+            virtual size_t numVertices(void) const = 0;
+            virtual const float* vertices(void) const = 0;  // Size is 3 * numVertices();
+            virtual const float* uvcoords(void) const = 0;  // Size is 2 * numVertices();
+            virtual const float* normals(void) const = 0;   // Size is 3 * numVertices();
+
+        };
+
+        class MeshRaw : Mesh {
+
+        public:
+            std::vector<float> m_vertices, m_uvcoords, m_normals;
+
+        public:
+            virtual size_t numVertices(void) const override;
+            virtual const float* vertices(void) const override;
+            virtual const float* uvcoords(void) const override;
+            virtual const float* normals(void) const override;
+
+        private:
+            bool checkSizeValidity(void) const;
+
+        };
+
+        class RenderUnit {
+
+        public:
+            std::unique_ptr<Mesh> m_mesh;
+            Material m_material;
+
+        };
+
+        class RenderUnitStatic {
+
+        public:
+            MeshRaw m_mesh;
+            Material m_material;
+
+        };
+
+        class Transform {
+
+        public:
+            glm::vec3 m_pos;
+            glm::quat m_quat;
+            float m_scale = 1.f;
+
+        };
+
+        class StaticActor {
+            
+        public:
+            std::string m_name;
+            Transform m_trans;
+
+        };
+
+    }
+
 }
